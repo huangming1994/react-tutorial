4c81404da7761e1888dddb6329a4c466d008c805
huangming1994 <1157114498@qq.com>
2017-09-05 20:45:13 +0800

    add middleware

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,3 +1,46 @@
-/**
- * @flow
- */
+import { applyMiddleware } from 'redux'
+// thunk中间件(函数action)
+export function thunk({ dispatch, getState }) {
+  return next => action => {
+    if (action && typeof action === 'function') {
+      return dispatch(action(getState()))
+    }
+    return next(action)
+  }
+}
+// promise中间件(promise action)
+export function promise({ dispatch }) {
+  return next => action => {
+    if (action && typeof action.then === 'function') {
+      const resolve = (input) => dispatch(input)
+      return action.then(resolve).catch(resolve)
+    }
+    return next(action)
+  }
+}
+
+// 数组中间件(多个action的数组)
+export function multiDispatch({ dispatch }) {
+  return next => actions => {
+    if (Array.isArray(actions)) {
+      return actions.map((action) => dispatch(action))
+    }
+    return next(actions)
+  }
+}
+
+export function logger({ getState }) {
+  return next => action => {
+    console.log('prev state', getState())
+    console.log('action', action)
+    const nextState = next(action)
+    console.log('next state', getState())
+    return nextState
+  }
+}
+export default applyMiddleware(
+  multiDispatch,
+  promise,
+  thunk,
+  logger,
+)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers } from 'redux'
 import { numReducer } from './reducer'
+import enhancer from './middleware'
 
 const reducers = combineReducers({
   num: numReducer,
 })
-export const store = createStore(reducers)
+export const store = createStore(reducers, undefined, enhancer)
